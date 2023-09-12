221de76a8ec534764909efc2e81a8b4a73dd0593
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2023-09-12 16:07:09 -0600

    update method

--- src/data/httpClient.js
+++ src/data/httpClient.js
@@ -2,6 +2,7 @@
 const API = 'https://api.themoviedb.org/3/'
 export async function Get(path) {
     const result = await fetch(API + path, {
+        method: 'GET',
         headers: {
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs',
             'Content-Type': 'application/json;charset=utf-8'
