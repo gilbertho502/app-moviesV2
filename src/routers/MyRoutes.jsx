b5d638eece6c39407f22db86431f24ed306674d6
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2025-09-05 20:33:36 -0600

    refactor MyRoutes to update movie and series routes with new components

--- src/routers/MyRoutes.jsx
+++ src/routers/MyRoutes.jsx
@@ -1,13 +1,20 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {LandingPage} from '../pages/LandingPage'
-import { Detalles } from '../pages/Detalles'
+import {DetallesMovies} from '../pages/DetallesMovies'
+import {DetallesSeries} from '../pages/DetallesSeries'
+import {MoviesPage} from '../pages/MoviesPage'
+import {SeriesPage} from '../pages/SeriesPage'
 
 const MyRoutes = ()=>{
     return(
         <Router basename='/'>
             <Routes>
                 <Route exact path='/' element={<LandingPage/>}/>
-                <Route exact path='/movies/:movieId' element={<Detalles/>}></Route>
+                <Route exact path='/movies' element={<MoviesPage/>}/>
+                <Route exact path='/tv' element={<SeriesPage/>}/>
+                <Route exact path='/movies/:movieId' element={<DetallesMovies/>}></Route>
+                <Route exact path='/tv/:tvId' element={<DetallesSeries/>}></Route>
+                <Route path='*' element={<h1>404 Not Found</h1>}/>
             </Routes>
         </Router>
     )
