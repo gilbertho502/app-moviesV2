1c8967f2e90cb5e3057cd76a302c77934a7d4777
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2025-09-05 20:33:23 -0600

    refactor LandingPage to replace ContextCard with movie and series cards

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,31 @@
-import { ContextCard } from '../components/ContextCard'
+import { Card, Col, Row } from "antd";
+import camera from "../assets/camera.png"
+import series from "../assets/television.png"
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
-    return(
-        <div>
-            <ContextCard/>
-        </div>
-    )
+  return (
+    <div>
+  <Row gutter={24} 
+        justify="center" 
+        style={{ marginTop: 20 }}>
+    <Col span={9}>
+      <Card title="Movies" variant="borderless">
+         <Link to = {'/movies'}>
+        <img src={camera} alt="camera" width={300} />
+        </Link>
+      </Card>
+    </Col>
+    <Col span={9}>
+      <Card title="Series Tv" variant="borderless">
+        <Link to = {'/tv'}>
+        <img src={series} alt="series" width={300} />
+        </Link>
+      </Card>
+    </Col>
+  </Row>
+    </div>
+  );
 };
 
-export {LandingPage}
+export { LandingPage };
