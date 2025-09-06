8df712f3937071e48a933f837168553c9ea20b8a
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2025-09-05 20:32:27 -0600

    update header title to include 'Movies & TV Series 2025'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import MyRoutes from './routers/MyRoutes'
 
+
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
+
         <h1 className='title'>
-          🎬 Movies 2025
+          🎬 Movies & TV Series 2025
         </h1>
+
       </header>
       <MyRoutes/>
     </div>
