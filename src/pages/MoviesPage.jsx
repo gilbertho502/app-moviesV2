2c19d18ce9f76c2427c648f421145390ec54b5a6
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2025-09-05 20:33:46 -0600

    add MoviesPage and SeriesPage components with respective ContextMovieCard and ContextSerieCard

---
+++ src/pages/MoviesPage.jsx
@@ -0,0 +1,11 @@
+import { ContextMovieCard } from "../components/ContextMovieCard";
+
+const MoviesPage = () => {
+  return (
+    <div>
+      <ContextMovieCard />   
+    </div>
+  );
+};
+
+export { MoviesPage };
---
+++ src/pages/SeriesPage.jsx
@@ -0,0 +1,11 @@
+import { ContextSerieCard } from "../components/ContextSerieCard";
+
+const SeriesPage = () => {
+  return (
+    <div>
+      <ContextSerieCard />
+    </div>
+  );
+};
+
+export { SeriesPage };
