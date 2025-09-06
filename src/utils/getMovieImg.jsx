5a3acf3980794ad5a5c53de42c4fbebb42aa3cd4
Gilberto <66382832+gilbertho502@users.noreply.github.com>
2025-09-05 20:34:00 -0600

    add getSerieImg function to retrieve series images

--- src/utils/getMovieImg.jsx
+++ src/utils/getMovieImg.jsx
@@ -1,7 +1,10 @@
 import pelicula from '../img/pelicula.png'
-
 const getMovieImg =(path, width)=>{
     return path ? `https://image.tmdb.org/t/p/w${width}${path}` : pelicula;
 }
 
-export {getMovieImg}
+const getSerieImg =(path, width)=>{
+    return path ? `https://image.tmdb.org/t/p/w${width}${path}` : pelicula; 
+}
+
+export {getMovieImg, getSerieImg}
